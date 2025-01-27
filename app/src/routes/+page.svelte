154419aa0b7751e07487cbe5e5d79a685ef42321
m4rs <marcel@marcelclaramunt.cat>
2025-01-27 21:08:11 +0100

    random without replacement

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,10 +1,27 @@
+<script lang="ts" module>
+  const range = (end: number) => new Array(end).fill(0).map((_, i) => i)
+  function shuffle<T>(xs: T[]) {
+    for (let i = xs.length - 1; i >= 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [xs[i], xs[j]] = [xs[j], xs[i]];
+    }
+  }
+  const UNUSED = range(25)
+  shuffle(UNUSED)
+</script>
+
 <script lang='ts'>
   import Desktop from './Desktop.svelte'
   import Mobile from './Mobile.svelte'
 
+
+  let unused = $state(UNUSED)
+  // $inspect(unused)
+
   async function randomQuote() {
-    const end = 25
-    const index = Math.floor(Math.random() * end)
+    if (unused.length === 0)
+      unused = UNUSED
+    const index = unused.pop()!
     return await fetch(`/quotes/${index}.txt`).then(r => r.text())
   }
 
