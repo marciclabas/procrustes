7602ea8b4e60a2625519ae6eabe682cee1c9911a
m4rs <marcel@marcelclaramunt.cat>
2025-01-27 21:07:27 +0100

    static adapter

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -1,18 +1,17 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-export default {
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://svelte.dev/docs/kit/integrations
+	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter({
-			// See below for an explanation of these options
-			routes: {
-				include: ['/*'],
-				exclude: ['<all>']
-			},
-			platformProxy: {
-				environment: undefined,
-				experimentalJsonConfig: false,
-				persist: false
-			}
-		})
-	}
+			pages: 'build',
+      assets: 'build',
+		}),
+		appDir: 'app_'
+	},
 };
+
+export default config;
